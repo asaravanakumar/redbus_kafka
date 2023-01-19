71e81ace308ede45420b0bbae9337b239549469b
Saravana Kumar <saravana@Saravanas-MacBook-Pro.local>
2023-01-19 09:31:07 +0530

    kafka examples

---
+++ labs/examples/shopping-cart-order-service/src/test/java/com/examples/scart/order/OrderServiceMainTests.java
@@ -0,0 +1,16 @@
+package com.examples.scart.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+
+@SpringBootTest
+@EmbeddedKafka
+public class OrderServiceMainTests {
+
+	@Test
+	public void contextLoads() {
+		
+	}
+
+}
