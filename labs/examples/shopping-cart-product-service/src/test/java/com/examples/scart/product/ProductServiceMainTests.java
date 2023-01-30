a145b1be82b98f1ec12c2dd3c91354cdea627b55
Saravana Kumar <saravana@Saravanas-MacBook-Pro.local>
2023-01-30 19:33:17 +0530

    kafka training notes and streaming example

---
+++ labs/examples/shopping-cart-product-service/src/test/java/com/examples/scart/product/ProductServiceMainTests.java
@@ -0,0 +1,14 @@
+package com.examples.scart.product;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductServiceMainTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
